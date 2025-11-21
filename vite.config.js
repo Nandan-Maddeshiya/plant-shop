c881e32e670eb6e143bdbbf549baacdfa328d091
Nandan-Maddeshiya <nandanmaddeshiya1@gmail.com>
2025-11-22 00:21:01 +0530

    fix: vite build for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // force modern target so Rollup optional native binary issue is avoided on Vercel
+    target: 'esnext'
+  }
 })
